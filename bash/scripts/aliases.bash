b73257b4a54f5e5c0821b78295a2c2dccdc7bf88
Pete Kinnecom <pete.kinnecom@appfolio.com>
2013-06-08 08:03:12 -0700

    update gitignore, migrate alias

--- bash/scripts/aliases.bash
+++ bash/scripts/aliases.bash
@@ -151,3 +151,7 @@ function color_test()
 {
   for code in $(seq -w 0 255); do for attr in 0 1; do printf "%s-%03s %bTest%b\n" "${attr}" "${code}" "\e[${attr};38;05;${code}m" "\e[m"; done; done | column -c $((COLUMNS*2))
 }
+function migrate()
+{
+  brake db:migrate; RAILS_ENV=test brake db:migrate
+}
